4d14581d2c846ad3241896ed7e73e11376956e8b
Vladimir Nuñez <vnc3xp@gmail.com>
2021-12-01 14:57:18 -0400

    initial commit

---
+++ MiPrimeraApp/App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ marginBottom: 20 }}>Hola Mundo en App.tsx React Native con TypeScript</Text>
+      <Text>Es genial esto, siiii</Text>
+    </View>
+  )
+}
+
+export default App;
+
+
---
+++ pruebats/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {
+  View,
+  Text,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hola desde el pixel 2</Text>
+    </View>
+  );
+};
+
+export default App;
